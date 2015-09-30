724b4d6e8c639180cee0e8e6c45e264613bba72d
craigbrennan24 <craig_brennan24@hotmail.com>
2015-09-30 13:49:51 +0000

    Initial commit for palindrome problem

---
+++ Lab03/palindrome.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+#Program that tests if input string is a palindrome, returns TRUE or FALSE
+
+def Palindrome( s ):
+	string = str(s)
+	string.upper()
+
+	if( s == "TEST" ):
+		print "TRUE"
+	else:
+		print "FALSE"
+
