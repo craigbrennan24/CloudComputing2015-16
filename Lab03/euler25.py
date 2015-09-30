a7f57b81f62cfc4724c012172a9ae2c54fac1f53
craigbrennan24 <craig_brennan24@hotmail.com>
2015-09-30 16:03:41 +0000

    Finished euler 25 project

--- Lab03/euler25.py
+++ Lab03/euler25.py
@@ -1,10 +1,24 @@
 #! /usr/bin/env python
 
-def 1kDigFibonacci():
+def DigitFibonacci():
 	fib1 = 0
 	fib2 = 1
 	temp = 0
 
-	found = false
+	found = False
+	fibCounter = 1
+	while( found == False ):
+		fibStr = str(fib2)
+		fibCheck = len(fibStr)
+		if( fibCheck >= 1000 ):
+			found = True
+		else:
+			temp = fib2
+			fib2 = fib2 + fib1
+			fib1 = temp
+			fibCounter += 1
 
-1kDigFibonacci()
+	print "First 1k Digit Fib num = %d" % fib2
+	print "Fib index = %d" % fibCounter
+
+DigitFibonacci()
